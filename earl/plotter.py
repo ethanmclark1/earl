e7a2346402890a037acd2680c027d03052bb8a04
Ethan <eclark715@gmail.com>
2023-09-18 09:24:34 -0700

    Final results

--- earl/plotter.py
+++ earl/plotter.py
@@ -20,8 +20,12 @@ def plot_metrics(problem_instances, metrics):
         plt.bar(x_values[1], a_star, width=0.4, label='A*')
         plt.xlabel(problem_instance.capitalize())
         plt.xticks(x_values, labels=names)
-        plt.ylim(0, 25)
+        plt.ylim(0, 10)
         
         plt.tight_layout(rect=[0, 0.03, 1, 0.95])
         plt.savefig(f'{results_dir}/{problem_instance}.png')
         plt.clf()
+        
+        print(f'{problem_instance}: {metrics[idx]}')
+        
+        
