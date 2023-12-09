7f4a8a9c00272352d54db48683b4a2a4ff28ccf3
Ethan <eclark715@gmail.com>
2023-12-08 17:34:28 -0700

    Fixed reward function to now include the utility

--- earl/agents/utils/ea.py
+++ earl/agents/utils/ea.py
@@ -8,28 +8,24 @@ import networkx as nx
 
 
 class EA:
-    def __init__(self, env, problem_space, num_obstacles):
+    def __init__(self, env, rng):
         self._init_hyperparams()
         
         self.env = env
-        self.problem_space = problem_space
+        self.rng = rng
+        
         self.num_cols = env.unwrapped.ncol
-        self.num_obstacles = num_obstacles
-        
         self.grid_dims = env.unwrapped.desc.shape
-        self.grid_size = '4x4' if self.grid_dims[0] == 4 else '8x8'
         self.state_dims = env.observation_space.n
-        # Add a dummy action (+1) to terminate the episode
-        self.action_dims = 4 + 1 if problem_space == 'small' else env.observation_space.n + 1
-        
-        self.random_seed = 42
-        self.rng = np.random.default_rng(seed=self.random_seed)
+        self.grid_size = '4x4' if self.grid_dims[0] == 4 else '8x8'
         
     def _init_hyperparams(self):
-        self.action_cost = 0.50
+        self.max_actions = 10
+        self.action_cost = 0.35
         self.sma_percentage = 0.05
-        self.configs_to_consider = 100
-        self.action_success_rate = 0.25
+        self.percent_obstacles = 0.75
+        self.configs_to_consider = 25
+        self.action_success_rate = 0.75
 
     def _save(self, approach, problem_instance, adaptation):
         directory = f'earl/agents/history/{approach.lower()}'
@@ -59,99 +55,85 @@ class EA:
         config = wandb.config
         return config
     
-    def _transform_action(self, action):
-        if self.problem_space == 'small':
-            # Shift action indices to the middle 4 cells of the grid
-            # 0 -> 5; 1 -> 6; 2 -> 9; 3 -> 10
-            shift = 5
-            if action == 2 or action == 3:
-                shift += 2
-            action += shift
-        return action     
-    
     def _create_graph(self):
         graph = nx.grid_graph(dim=[self.num_cols, self.num_cols])
         nx.set_edge_attributes(graph, 1, 'weight')
         return graph
     
-    # Calculate rewards for a given configuration by averaging A* path lengths over multiple trials
+    # Calculate utilities for a given configuration by averaging A* path lengths over multiple trials
     """
     Cell Values:
-        0: Safe
+        0: Frozen
         1: Bridge
         2: Start
         3: Goal
         4: Hole
     """
-    def _get_reward(self, problem_instance, configuration, num_action):
+    def _calc_utility(self, problem_instance, configuration):
         def manhattan_dist(a, b):
             return abs(a[0] - b[0]) + abs(a[1] - b[1])
         
-        bridge_bonus = 2
-        valid_path_reward = 20
-        invalid_path_penalty = -20
-        path_length_penalty = 1
-
-        rewards = []
+        utilities = []
+        graph = self._create_graph()
         desc = copy.deepcopy(configuration).reshape(self.grid_dims)
         row_idx, col_idx = np.where(desc == 1)
-        bridges = set(zip(row_idx, col_idx))
         
-        paths = []
+        bridges = set(zip(row_idx, col_idx))
+        for bridge in bridges:
+            bridge = tuple(bridge)
+            for neighbor in graph.neighbors(bridge):
+                graph[bridge][neighbor]['weight'] = 0
+        
         for _ in range(self.configs_to_consider):
-            path = None
-            graph = self._create_graph()
-            start, goal, obstacles = problems.get_entity_positions(problem_instance, self.grid_size, self.num_obstacles)
+            tmp_desc = desc.copy()
+            tmp_graph = graph.copy()
+            start, goal, obstacles = problems.get_entity_positions(problem_instance, self.rng, self.grid_size, self.percent_obstacles)
             
             # Bridges cannot cover start or goal cells 
-            if desc[start] == 1 or desc[goal] == 1:
-                rewards += [invalid_path_penalty]
+            if tmp_desc[start] == 1 or tmp_desc[goal] == 1:
+                utilities += [-self.state_dims]
                 continue
             
-            desc[start], desc[goal] = 2, 3
-            # Populate obstacle in cell only if bridge is not already there
+            tmp_desc[start], tmp_desc[goal] = 2, 3
+            #  obstacle in cell only if bridge is not already there
             for obstacle in obstacles:
                 obstacle = tuple(obstacle)
-                if desc[obstacle] != 1:
-                    desc[obstacle] = 4
-                    graph.remove_node(obstacle)
+                if tmp_desc[obstacle] != 1:
+                    tmp_desc[obstacle] = 4
+                    tmp_graph.remove_node(obstacle)
             
-            for bridge in bridges:
-                bridge = tuple(bridge)
-                for neighbor in graph.neighbors(bridge):
-                    graph[bridge][neighbor]['weight'] = 0
-            
-            try:          
-                path = nx.astar_path(graph, start, goal, manhattan_dist, 'weight')
-                reward = valid_path_reward - (path_length_penalty * len(path))
-                bridges_used = len(set(path) & bridges)
-                reward += (bridge_bonus * bridges_used)
-                action_penalty = self.action_cost * num_action
-                reward -= action_penalty
-                rewards += [reward]
-            except nx.NetworkXNoPath:
-                rewards += [invalid_path_penalty]
-                
-            desc = copy.deepcopy(configuration).reshape(self.grid_dims)
+            path = nx.astar_path(tmp_graph, start, goal, manhattan_dist, 'weight')
+            utility = len(set(path) - bridges)
+            utilities += [-utility]
 
-        avg_reward = np.mean(rewards)
-        return avg_reward
+        avg_utility = np.mean(utilities)
+        return avg_utility
     
+    # r(s,a,s') = u(s') - u(s) - c(a)
+    def _get_reward(self, problem_instance, state, done, next_state, num_action):
+        if not np.array_equal(state, next_state):
+            util_s = self._calc_utility(problem_instance, state)
+            util_s_prime = self._calc_utility(problem_instance, next_state)
+            reward = util_s_prime - util_s - (self.action_cost * num_action)
+        elif not done:
+            reward = -self.action_cost * num_action
+        else:
+            reward = 0
+            
+        return reward
+        
     # Apply adaptation to task environment
     def _step(self, problem_instance, state, action, num_action):
-        reward = 0
         state = copy.deepcopy(state)
         terminating_action = self.action_dims - 1
         
-        next_state = state
+        next_state = state.copy()
         # Add stochasticity to instrumental action execution
         if action != terminating_action and self.action_success_rate > self.rng.random():
-            action = self._transform_action(action)
             next_state[action] = 1
         
-        done = action == terminating_action
-        if done:
-            reward = self._get_reward(problem_instance, next_state, num_action)  
+        done = action == terminating_action or num_action == self.max_actions
+        reward = self._get_reward(problem_instance, state, done, next_state, num_action)  
         
         return reward, next_state, done
     
