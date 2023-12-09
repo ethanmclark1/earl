262f473ce998ee1e02dc9052fdcc07f1b078cb4b
Ethan <eclark715@gmail.com>
2023-12-08 17:36:39 -0700

    Cleaned up the problems and arguments

--- earl/arguments.py
+++ earl/arguments.py
@@ -5,26 +5,22 @@ def get_arguments():
         description='Teach a multi-agent system to create its own context-dependent language.'
         )
     
-    # --problem_space determines the size of the grid & the number of possible actions
-    # 'small' = 4x4 grid with 4 possible actions (center 2x2 square)
-    # 'medium' = 4x4 grid with 16 possible actions
-    # 'large' = 8x8 grid with 64 possible actions
     parser.add_argument(
-        '--problem_space',
-        type=str, 
-        default='small',
-        choices=['small', 'medium', 'large'],
-        help='Size of the problem space {default_val: small, choices: [%(choices)s]}'
+        '--grid_size',
+        type=str,
+        default='4x4',
+        choices=['4x4', '8x8'],
+        help='Size of the grid {default_val: 4x4, choices: [%(choices)s]}'
         )
-    
+            
     parser.add_argument(
         '--render_mode', 
         type=str, 
         default='human', 
-        choices=['human', 'ansi', 'rgb_array'], 
+        choices=['human', 'ansi', 'rgb_array', 'None'], 
         help='Mode of visualization {default_val: None, choices: [%(choices)s]}'
         )
     
     args = parser.parse_args()
         
-    return args.problem_space, args.render_mode
+    return args.grid_size, args.render_mode
