759a7bc6718dc6edada2f2df9606a8ecf0fda3c9
Ethan <eclark715@gmail.com>
2023-12-08 17:35:19 -0700

    Includede get_adaptation to retrieve final adaptations

--- earl/agents/lfa.py
+++ earl/agents/lfa.py
@@ -9,26 +9,29 @@ from agents.utils.ea import EA
 
 # Linear Function Approximation
 class BasicLFA(EA):
-    def __init__(self, env, grid_size, num_obstacles):
-        super(BasicLFA, self).__init__(env, grid_size, num_obstacles)
+    def __init__(self, env, rng):
+        super(BasicLFA, self).__init__(env, rng)
         
         self.weights = None
+        # Add a dummy action (+1) to terminate the episode
+        self.action_dims = env.observation_space.n + 1
         self.num_features = (2*self.state_dims) + self.action_dims
         
-        self.alpha = 0.0005
+        self.alpha = 0.0003
         self.epsilon_start = 1
-        self.num_episodes = 10000
-        self.epsilon_decay = 0.9997
+        self.num_episodes = 7500
+        self.epsilon_decay = 0.999
         self.sma_window = int(self.num_episodes * self.sma_percentage)
 
     def _init_wandb(self, problem_instance):
         config = super()._init_wandb(problem_instance)
         config.alpha = self.alpha
-        config.epsilon = self.epsilon_start
         config.action_cost = self.action_cost
         config.num_episodes = self.num_episodes
         config.epsilon_decay = self.epsilon_decay
+        config.percent_obstacles = self.percent_obstacles
         config.action_success_rate = self.action_success_rate
+        config.configs_to_consider = self.configs_to_consider
         
     def _get_max_q_value(self, state):
         best_action = None
@@ -86,6 +89,7 @@ class BasicLFA(EA):
             done = False
             num_action = 0
             action_seq = []
+            episode_reward = 0
             state = np.zeros(self.state_dims, dtype=int)
             while not done:
                 num_action += 1
@@ -95,34 +99,46 @@ class BasicLFA(EA):
                 self._update_weights(state, action, reward, next_state, done)
                 state = next_state
                 action_seq += [action]
+                episode_reward += reward
                 
             self.epsilon *= self.epsilon_decay
             
             rewards.append(reward)
             avg_rewards = np.mean(rewards[-self.sma_window:])
             wandb.log({"Average Reward": avg_rewards})
-            
+    
     def _get_adaptation(self, problem_instance):
-        done = False
-        num_action = 0
-        action_seq = []
-        self.epsilon = 0
-        state = np.zeros(self.state_dims, dtype=int)
-        while not done:
-            num_action += 1
-            action = self._select_action(state)
-            reward, next_state, done = self._step(problem_instance, state, action, num_action)
-            state = next_state
-            action_seq += [action]
+        trials = 25
+        scoreboard = set()
+        
+        for _ in range(trials):
+            done = False
+            num_action = 0
+            action_seq = []
+            episode_reward = 0
+            state = np.zeros(self.state_dims, dtype=int)
+            while not done:
+                num_action += 1
+                action = self._select_action(state)
+                reward, next_state, done = self._step(problem_instance, state, action, num_action)
+                state = next_state
+                action_seq += [action]
+                episode_reward += reward
+            
+            info = tuple(action_seq) + (episode_reward,) 
+            scoreboard.add(info)
+        
+        info = max(scoreboard, key=lambda x: x[-1])
+        action_seq = list(info[:-1])
+        reward = info[-1]
         
         return action_seq, reward
             
     def _generate_adaptations(self, problem_instance):
-        self._init_wandb(problem_instance) 
-        
         self.epsilon = self.epsilon_start
         self.weights = np.zeros(self.num_features)
                 
+        self._init_wandb(problem_instance) 
         self._train(problem_instance)
         adaptation, reward = self._get_adaptation(problem_instance)
         
@@ -134,10 +150,9 @@ class BasicLFA(EA):
     
 
 class HallucinatedLFA(BasicLFA):
-    def __init__(self, env, grid_size, num_obstacles):
-        super(HallucinatedLFA, self).__init__(env, grid_size, num_obstacles)
-        
-        self.max_seq_len = 9
+    def __init__(self, env, rng):
+        super(HallucinatedLFA, self).__init__(env, rng)
+        self.max_seq_len = 5
         
     def _sample_permutations(self, action_seq):
         permutations = {}
@@ -152,10 +167,10 @@ class HallucinatedLFA(BasicLFA):
                 permutations[tuple(permutation + (terminating_action,))] = None
         else:
             i = 1
-            max_permutations = math.factorial(self.max_seq_len)
-            while i < max_permutations:
+            max_samples = math.factorial(self.max_seq_len)
+            while i < max_samples:
                 random.shuffle(tmp_action_seq)
-                permutations[tuple(tmp_action_seq + [terminating_action])] = None
+                permutations[tuple(tmp_action_seq + (terminating_action,))] = None
                 i += 1   
         
         return list(permutations.keys())   
@@ -204,32 +219,46 @@ class HallucinatedLFA(BasicLFA):
             
     
 class CommutativeLFA(BasicLFA):
-    def __init__(self, env, grid_size, num_obstacles):
-        super(CommutativeLFA, self).__init__(env, grid_size, num_obstacles)
+    def __init__(self, env, rng):
+        super(CommutativeLFA, self).__init__(env, rng)
         
+        # (s, a) -> (s', r)
         self.ptr_lst = {}
         self.previous_sample = None
         
+    def _get_state_idx(self, state):
+        binary_str = "".join(str(cell) for cell in reversed(state))
+        state_idx = int(binary_str, 2)
+        return state_idx   
+    
+    def _get_state(self, state_idx):
+        binary_str = bin(state_idx)[2:]
+        state = np.zeros(self.state_dims, dtype=int)
+        for i, cell in enumerate(reversed(binary_str)):
+            state[i] = int(cell)
+        return state
+    
     def _update_weights(self, state, action, reward, next_state, done):
         super()._update_weights(state, action, reward, next_state, done)
         
         if self.previous_sample is None:
-            s = state
+            s = self._get_state_idx(state)
             a = action
             r_0 = reward
-            s_1 = next_state
+            s_1 = self._get_state_idx(next_state)
             
             self.previous_sample = (s, a, r_0)
             self.ptr_lst[(s, a)] = (s_1, r_0)
         else:
             s, a, r_0 = self.previous_sample
-            s_1 = state
+            s_1 = self._get_state_idx(state)
             b = action
             r_1 = reward
             s_prime = next_state
-            
+                        
             if (s, b) in self.ptr_lst:
                 s_2, r_2 = self.ptr_lst[(s, b)]
+                s_2 = self._get_state(s_2)
                 
                 current_features = self._extract_features(s_2, a)
                 current_q_value = np.dot(self.weights, current_features)
@@ -239,7 +268,8 @@ class CommutativeLFA(BasicLFA):
                 td_error = td_target - current_q_value
                 
                 self.weights = self.weights + self.alpha * td_error * current_features
-                
+            
+            s_prime = self._get_state_idx(s_prime)
             self.previous_sample = (s_1, b, r_1)
             self.ptr_lst[(s_1, b)] = (s_prime, r_1)
     
--- earl/agents/q_table.py
+++ earl/agents/q_table.py
@@ -9,39 +9,55 @@ from agents.utils.ea import EA
 
 
 class BasicQTable(EA):
-    def __init__(self, env, problem_space, num_obstacles):
-        super(BasicQTable, self).__init__(env, problem_space, num_obstacles)
-        
+    def __init__(self, env, rng):
+        super(BasicQTable, self).__init__(env, rng)
+                
         self.q_table = None
+        # Add a dummy action (+1) to terminate the episode
+        self.action_dims = 4 + 1
         self.nS = 2 ** self.state_dims
         
         self.alpha = 0.0005
         self.epsilon_start = 1
-        self.num_episodes = 10000
+        self.num_episodes = 7500
         self.epsilon_decay = 0.999
         self.sma_window = int(self.num_episodes * self.sma_percentage)
         
     def _init_wandb(self, problem_instance):
         config = super()._init_wandb(problem_instance)
         config.alpha = self.alpha
-        config.epsilon = self.epsilon_start
         config.action_cost = self.action_cost
         config.num_episodes = self.num_episodes
         config.epsilon_decay = self.epsilon_decay
+        config.percent_obstacles = self.percent_obstacles
         config.action_success_rate = self.action_success_rate
+        config.configs_to_consider = self.configs_to_consider
 
     def _get_state_idx(self, state):
         binary_str = "".join(str(cell) for cell in reversed(state))
         state_idx = int(binary_str, 2)
         return state_idx   
+
+    # Shift action indices to the middle 4 cells of the grid
+    # 0 -> 5; 1 -> 6; 2 -> 9; 3 -> 10; 4 -> 4
+    def _transform_action(self, action):
+        shift = 5
+        # Terminating action is unchanged
+        if action != 4:
+            if action == 2 or action == 3:
+                shift += 2
+            action += shift
+        return action     
         
     def _select_action(self, state):
         if self.rng.random() < self.epsilon:
-            action = self.rng.integers(self.action_dims)
+            original_action = self.rng.integers(self.action_dims)
         else:
             state_idx = self._get_state_idx(state)
-            action = self.q_table[state_idx].argmax()
-        return action
+            original_action = self.q_table[state_idx].argmax()
+            
+        transformed_action = self._transform_action(original_action)
+        return transformed_action, original_action
     
     def _update_q_table(self, state, action, reward, next_state, done):
         action_idx = action
@@ -60,43 +76,56 @@ class BasicQTable(EA):
             done = False
             num_action = 0
             action_seq = []
+            episode_reward = 0
             state = np.zeros(self.state_dims, dtype=int)
             while not done:
                 num_action += 1
-                action = self._select_action(state)
-                reward, next_state, done = self._step(problem_instance, state, action, num_action)
+                transformed_action, original_action = self._select_action(state)
+                reward, next_state, done = self._step(problem_instance, state, transformed_action, num_action)
                 
-                self._update_q_table(state, action, reward, next_state, done)
+                self._update_q_table(state, original_action, reward, next_state, done)
                 state = next_state
-                action_seq += [action]
+                action_seq += [original_action]
+                episode_reward += reward
                 
             self.epsilon *= self.epsilon_decay
             
-            rewards.append(reward)
+            rewards.append(episode_reward)
             avg_rewards = np.mean(rewards[-self.sma_window:])
             wandb.log({"Average Reward": avg_rewards})
                         
     def _get_adaptation(self, problem_instance):
-        done = False
-        num_action = 0
-        action_seq = []
-        self.epsilon = 0
-        state = np.zeros(self.state_dims, dtype=int)
-        while not done:
-            num_action += 1
-            action = self._select_action(state)
-            reward, next_state, done = self._step(problem_instance, state, action, num_action)
-            state = next_state
-            action_seq += [action]
+        trials = 25
+        scoreboard = set()
+        
+        for _ in range(trials):
+            done = False
+            num_action = 0
+            action_seq = []
+            episode_reward = 0
+            state = np.zeros(self.state_dims, dtype=int)
+            while not done:
+                num_action += 1
+                transformed_action, original_action = self._select_action(state)
+                reward, next_state, done = self._step(problem_instance, state, transformed_action, num_action)
+                state = next_state
+                action_seq += [original_action]
+                episode_reward += reward
+            
+            info = tuple(action_seq) + (episode_reward,) 
+            scoreboard.add(info)
+        
+        info = max(scoreboard, key=lambda x: x[-1])
+        action_seq = list(info[:-1])
+        reward = info[-1]
         
         return action_seq, reward
         
     def _generate_adaptations(self, problem_instance):
-        self._init_wandb(problem_instance)
-        
         self.epsilon = self.epsilon_start
         self.q_table = np.zeros((self.nS, self.action_dims))
         
+        self._init_wandb(problem_instance)
         self._train(problem_instance)
         adaptation, reward = self._get_adaptation(problem_instance)
         
@@ -108,11 +137,10 @@ class BasicQTable(EA):
 
 
 class HallucinatedQTable(BasicQTable):
-    def __init__(self, env, problem_space, num_obstacles):
-        super(HallucinatedQTable, self).__init__(env, problem_space, num_obstacles)
-        
-        self.max_seq_len = 9
-        
+    def __init__(self, env, rng):
+        super(HallucinatedQTable, self).__init__(env, rng)
+        self.max_seq_len = 5
+                
     def _sample_permutations(self, action_seq):
         permutations = {}
         permutations[tuple(action_seq)] = None
@@ -178,8 +206,8 @@ class HallucinatedQTable(BasicQTable):
     
     
 class CommutativeQTable(BasicQTable):
-    def __init__(self, env, problem_space, num_obstacles):
-        super(CommutativeQTable, self).__init__(env, problem_space, num_obstacles)
+    def __init__(self, env, rng):
+        super(CommutativeQTable, self).__init__(env, rng)
         
         # (s, a) -> (s', r)
         self.ptr_lst = {}
