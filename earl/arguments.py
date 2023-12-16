580c6d06e01abf5e66bc2f8465f8e7fe09e24156
Ethan <eclark715@gmail.com>
2023-12-15 19:54:33 -0700

    removed grid_size argument

--- earl/arguments.py
+++ earl/arguments.py
@@ -4,21 +4,6 @@ def get_arguments():
     parser = argparse.ArgumentParser(
         description='Teach a multi-agent system to create its own context-dependent language.'
         )
-    
-    parser.add_argument(
-        '--grid_size',
-        type=str,
-        default='4x4',
-        choices=['4x4', '8x8'],
-        help='Size of the grid {default_val: 4x4, choices: [%(choices)s]}'
-        )
-    
-    parser.add_argument(
-        '--has_max_action',
-        type=bool,
-        default=False,
-        help='Whether to place a cap on the number of instrumental actions {default_val: False}'
-        )
             
     parser.add_argument(
         '--render_mode', 
@@ -30,4 +15,4 @@ def get_arguments():
     
     args = parser.parse_args()
         
-    return args.grid_size, args.has_max_action, args.render_mode
+    return args.render_mode
