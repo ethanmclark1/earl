af896807561bd5d68bec856663931072bdb72ebf
Ethan <eclark715@gmail.com>
2024-03-04 14:03:20 -0700

    Moved problem instances into yaml file

--- earl/problems.py
+++ earl/problems.py
@@ -1,53 +1,103 @@
-desc = [
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-]
+import yaml
+import networkx as nx
 
-
-problems = {
-    'citycenter': {
-        'starts': [(0,0),(0,1),(0,4),(0,7),(1,0),(1,1),(1,3),(1,7),(2,0),(2,3),(3,0),(3,2),(3,5),(4,0),(5,4),(7,1),(7,2),(7,4)],
-        'goals': [(0,5),(2,4),(3,3),(3,4),(4,2),(4,3),(4,4),(4,5),(5,5),(7,0)],
-        'holes': [            (0,2),(0,3),            (0,6),
-                              (1,2),      (1,4),
-                        (2,1),(2,2),            (2,5),(2,6),
-                        (3,1),                        (3,6),
-                                                      (4,6),
-                        (5,1),(5,2),(5,3),            (5,6),
-                        (6,1),(6,2),(6,3),(6,4),              
-                                    (7,3),            (7,6),(7,7)]
-    },
-    'pathway': {
-        'starts': [(0,0),(0,1),(0,2),(0,5),(0,6),(0,7),(1,6),(1,7),(2,7),(5,7),(6,6),(6,7),(7,0),(7,1),(7,5),(7,6),(7,7)],
-        'goals': [(1,0),(1,3),(2,0),(2,1),(2,2),(3,0),(3,7),(4,0),(4,1),(4,2),(4,7),(5,1),(6,2),(7,3)],
-        'holes': [     (1,1),(1,2),      (1,4),(1,5),
-                                   (2,3),      (2,5),(2,6),
-                       (3,1),(3,2),(3,3),(3,4),(3,5),(3,6),
-                                               (4,5),(4,6),
-                  (5,0),     (5,2),      (5,4),(5,5),(5,6),
-                  (6,0),(6,1),           (6,4),(6,5),
-                                         (7,4)]
-    }
+desc = {
+    '4x4': 
+        [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ],
+    '8x8':
+        [
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+        ]
 }
 
+output_file = 'earl/agents/utils/problems.yaml'
 
-def get_problem_list():
-    return list(problems.keys())
+def generate_problems(problem_size, rng, num_instances):
+    if problem_size == '8x8':
+        return
+    
+    with open(output_file, 'r') as file:
+        problem_instances = yaml.safe_load(file) or {}
+        
+    data = problem_instances.get(problem_size, {})
+    if len(data.keys()) == num_instances:
+        return
+    elif len(data.keys()) > num_instances:
+        for i in range(num_instances, len(data.keys())):
+            data.pop(f'instance_{i}')
+    else:    
+        flattened_list = [y for x in desc[problem_size] for y in x]
+        
+        problems = {}
+        i = len(data.keys())
+        grid_size = (4, 4)
+        num_starts = rng.integers(1, 3)
+        num_goals = rng.integers(1, 5)
+        num_holes = rng.integers(1, 6)
+        all_positions = [[i, j] for i in range(grid_size[0]) for j in range(grid_size[1])]
+        while i < num_instances:
+            tmp_positions = all_positions.copy()
+            entities_idx = rng.choice(len(flattened_list), size=num_starts+num_goals+num_holes, replace=False)
+            entities = list(map(lambda x: [int(x // grid_size[0]), int(x % grid_size[1])], entities_idx))  
+            start = entities[:num_starts]
+            goal = entities[num_starts:num_starts+num_goals]
+            holes = entities[num_starts+num_goals+1:]
+            for pos in start + goal:
+                tmp_positions.remove(pos)
+            mapping_positions = rng.choice(len(tmp_positions), size=4, replace=False)
+            mapping = {i: [int(pos // grid_size[0]), int(pos % grid_size[1])] for i, pos in enumerate(mapping_positions)}
+            
+            G = nx.grid_2d_graph(*grid_size)
+            for hole in holes:
+                hole = tuple(hole)
+                G.remove_node(hole)
+                
+            if all(nx.has_path(G, tuple(s), tuple(g)) for s in start for g in goal):
+                problem = {
+                    'starts': start,
+                    'goals': goal,
+                    'holes': holes,
+                    'mapping': mapping
+                }
+                problems[f'instance_{i}'] = problem
+                i += 1
 
-def get_entity_positions(problem_instance, rng, percent_holes):
-    starts = problems[problem_instance]['starts']
-    goals = problems[problem_instance]['goals']
+        problem_instances[problem_size] = data
+
+    with open(output_file, 'w') as file:
+        yaml.dump(problem_instances, file)
+
+def get_instance(problem_instance=None):
+    with open(output_file, 'r') as file:
+        problems = yaml.safe_load(file)
+    
+    if 'instance' in problem_instance:
+        instance = problems['4x4'][problem_instance]
+    else:
+        instance = problems['8x8'][problem_instance]
+    
+    return instance
+
+def get_entity_positions(instance, rng):
+    starts = instance['starts']
+    starts = [tuple(start) for start in starts]
     start = rng.choice(starts)
+    goals = instance['goals']
+    goals = [tuple(goal) for goal in goals]
     goal = rng.choice(goals)
-
-    holes = problems[problem_instance]['holes']
-    num_holes = int(len(holes) * percent_holes)
-    holes = rng.choice(holes, size=num_holes, replace=False)
+    
+    holes = instance['holes']
     
     return tuple(start), tuple(goal), holes
